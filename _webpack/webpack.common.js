25827cf9ac6ef221371a3264dfb208333b37b73d
vaishakk14 <vaishakk22@gmail.com>
2020-01-17 23:28:10 +0530

    feat: babel configuration webpack configuration project setup

--- _webpack/webpack.common.js
+++ _webpack/webpack.common.js
@@ -1,9 +1,15 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/client/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template: path.resolve(__dirname, '../_templates/client.html'),
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, '../_dist'),
     filename: '[name].bundle.js',
